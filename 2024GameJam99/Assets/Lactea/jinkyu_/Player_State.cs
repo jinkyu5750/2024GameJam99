e362e98e577962268072174dfe23d5f586bad371
jinkyu5750 <jinkyu57505750@gmail.com>
2024-11-09 22:19:35 +0900

    플레이어
    ㅇ

--- 2024GameJam99/Assets/Lactea/jinkyu_/Player_State.cs
+++ 2024GameJam99/Assets/Lactea/jinkyu_/Player_State.cs
@@ -1,20 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class Player_State : MonoBehaviour
 {
-
-    protected Animator ani;
+    public float HP = 100;
+    public Animator ani;
     protected Rigidbody2D rig;
     protected CapsuleCollider2D col;
     protected SpriteRenderer sr;
     [SerializeField] protected float moveSpeed;
+    [SerializeField] protected Slider HP_Slider;
     private void Start()
     {
         ani = GetComponent<Animator>();
         rig = GetComponent<Rigidbody2D>();
         col = GetComponent<CapsuleCollider2D>();
         sr = GetComponent<SpriteRenderer>();
+
+        HP_Slider.maxValue = HP;
+
+    }
+
+    private void Update()
+    {
+        HP -= Time.deltaTime * 0.5f;
+        HP_Slider.value = HP;
     }
 }
