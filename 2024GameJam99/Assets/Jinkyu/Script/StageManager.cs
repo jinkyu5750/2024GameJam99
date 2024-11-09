418e6807fbea80e58b62980d266e0a57889aca6f
jinkyu5750 <jinkyu57505750@gmail.com>
2024-11-10 03:30:48 +0900

    저장
    .

--- 2024GameJam99/Assets/Jinkyu/Script/StageManager.cs
+++ 2024GameJam99/Assets/Jinkyu/Script/StageManager.cs
@@ -7,7 +7,8 @@ public class StageManager : MonoBehaviour
 
 
     [Header("스테이지 프리팹")]
-    [SerializeField] private GameObject[] stage = new GameObject[3]; 
+    [SerializeField] private GameObject[] stage = new GameObject[3];
+    [SerializeField] private bool[] stageClear = { false, false, false }; // 클리어시 true로, 셋다 true면 엔딩 // 클리어했다는걸 표시할게필요함
     [SerializeField] private int currentStageIdx = -1; // -1인 경우는 스테이지선택씬,시작씬 , 사실 얘 있어야되는진 모르겠음아직
     public Canvas uiCanvas, inGameCanvas;
     public GameObject currentStage;
@@ -33,12 +34,12 @@ public class StageManager : MonoBehaviour
     {
         yield return new WaitForSeconds(duration);
 
-        if(inGameCanvas.transform.GetChild(2).gameObject.activeSelf == true) // ㅈ~~~ㄴ 레전드 코드 UIManager좀해놓지 ㅋ
+        if (inGameCanvas.transform.GetChild(2).gameObject.activeSelf == true) // ㅈ~~~ㄴ 레전드 코드 UIManager좀해놓지 ㅋ
         {
 
             inGameCanvas.transform.GetChild(2).transform.GetChild(0).transform.GetChild(3).transform.localScale = Vector3.zero;
             inGameCanvas.transform.GetChild(2).gameObject.SetActive(false);
-          
+
         }
         uiCanvas.gameObject.SetActive(true);
         inGameCanvas.gameObject.SetActive(false);
@@ -47,9 +48,12 @@ public class StageManager : MonoBehaviour
     public void InstantiateMap(int idx)
     {
         currentStageIdx = idx;
-        
-        GameObject go = Instantiate(stage[idx], Vector3.zero, Quaternion.identity);
-        currentStage = go;
+        GameObject go;
+        if (stageClear[idx] == false)
+        {
+            go = Instantiate(stage[idx], Vector3.zero, Quaternion.identity);
+            currentStage = go;
+        }
     }
 
 }
--- 2024GameJam99/Assets/Jinkyu/Script/StartButton.cs
+++ 2024GameJam99/Assets/Jinkyu/Script/StartButton.cs
@@ -8,11 +8,18 @@ using EasyTransition;
 
 public class StartButton : MonoBehaviour
 {
+    public AudioSource audioSource;
     public TransitionSettings transition;
     [SerializeField] private Ease ease;
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
     public void ClickStartButton()
     {
-       
+        audioSource.clip = SoundManager.instance.buttonClick;
+        audioSource.Play();
+
         Sequence seq = DOTween.Sequence();
         seq.Append(transform.DOScale(new Vector2(1.15f, 1.15f), 0.1f).SetEase(ease));
         seq.Append(transform.DOScale(new Vector2(0.95f, 0.95f), 0.1f).SetEase(ease));
